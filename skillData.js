97ad74d4591e83977cf2129c72b267589d57fb6f
Osivuetsu <105333434+Osivuetsu@users.noreply.github.com>
2025-03-07 04:36:18 +0100

    Add files via upload

---
+++ skillData.js
@@ -0,0 +1,46 @@
+const skillData = [
+    {
+        index: "01.",
+        skill: "BENG.COMPUTER IN VIEW",
+        src: "3star.svg",
+        ratingTxt: "3.0",
+        instiution: "Univerity of Benin (2021 till-date)",
+        Description: "I am currently pursuing my Bachelor of Engineering in Computer Engineering, gaining a strong foundation in programming, system design, and software development."
+
+    },
+
+    {
+        index: "01.",
+        skill: "FRONT-END DEVELOPMENT",
+        src: "4star.svg",
+        ratingTxt: "4.0",
+        instiution: "Edo Innovative Hub",
+        Description: "Proficient in developing responsive websites using technologies like React.js, HTML5, and CSS3 with a focus on user experience and modern designs."
+
+    },
+
+    {
+        index: "01.",
+        skill: "BACK-END DEVELOPMENT",
+        src: "3.5star.svg",
+        ratingTxt: "3.5",
+        instiution: " Edo Innovative Hub",
+        Description: "Skilled in building reliable and secure back-end systems using Node.js and PHP, ensuring seamless integration with front-end applications."
+
+    },
+
+    {
+        index: "01.",
+        skill: "DATA ANALYSIS",
+        src: "5star.svg",
+        ratingTxt: "5.0",
+        instiution: "Edo Innovative Hub",
+        Description: "Expert in analyzing datasets using tools like Python, SQL, and Power BI, turning data into actionable insights for improved decision-making."
+
+    },
+
+
+    
+]
+
+export default skillData
